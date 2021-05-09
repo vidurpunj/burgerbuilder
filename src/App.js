6b66869ced1f2eed7c57ad8dc6cfaf0507e53ff2
Vidur Punj <vidurt@tukatech.com>
2021-05-09 19:56:47 +0530

    Master update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {Component, useState} from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BugerBuilder/BugerBuilder';
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
-function App() {
-    return (
-        <div>
-            <AddUser/>
-            <Layout>
-                <BurgerBuilder></BurgerBuilder>
-            </Layout>
-        </div>
-    );
+class App extends Component {
+    state = {
+        users: [
+            {id: 1, name: 'John', age: 20},
+            {id: 2, name: 'Peter', age: 20},
+            {id: 3, name: 'Alex', age: 20},
+            {id: 4, name: 'Scean', age: 20},
+            {id: 5, name: 'Max', age: 20},
+            {id: 6, name: 'Million', age: 20},
+        ]
+    }
+     // [usersList, setUsersList] = useState([]);
+
+     addUserHandler = (uname, uage) => {
+        this.setState({users: [...this.state.users, {id: Math.random(), name: uname, age: uage}]})
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                <AddUser onAddUser={this.addUserHandler}/>
+                <UserList users={this.state.users}/>
+                <Layout>
+                    <BurgerBuilder></BurgerBuilder>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Classes from './AddUser.module.css';
 import Card from '../UI/Card';
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
+    const [enteredUserName, setEnteredUserName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
         console.log('New user created');
+        if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        if (+enteredAge > 1) { //+ensure it is a number
+            console.log(enteredUserName, enteredAge);
+            setEnteredUserName('');
+            setEnteredAge('');
+            props.onAddUser(enteredUserName, enteredAge);
+        }
     }
+
+    const userNameChangedHandler = (event) => {
+        setEnteredUserName(event.target.value);
+    }
+
+    const userAgeChangedHandler = (event) => {
+        setEnteredAge(event.target.value);
+    }
+
     return (
         <Card className={Classes.input}>
             <div>
                 <form method="post" onSubmit={addUserHandler}>
                     <div>
                         <label htmlFor="username">UserName:</label>
-                        <input type="text"></input>
-                        <label htmlFor="age">Age: </label>
-                        <input type="text"></input>
-                        <input type="submit"/>
+                        <input id="username"
+                               type="text"
+                               value={enteredUserName}
+                               onChange={userNameChangedHandler}></input>
+                        <label htmlFor="age">Age (years): </label>
+                        <input id="age"
+                               type="number"
+                               value={enteredAge}
+                               onChange={userAgeChangedHandler}></input>
+                        <Button type="submit">Add User</Button>
                     </div>
                 </form>
             </div>
